0228cbdebade58b63a4d2e3c62cfdac7b1d7aebf
Akhil Anilkumar <akhil.anilkumar@cubettech.com>
2024-10-24 11:08:56 +0530

    + assistant

--- stream.py
+++ stream.py
@@ -60,19 +60,16 @@ def query_chatbot(messages, model):
 def main():
     st.title("Chatbot Interface")
 
-
     if "messages" not in st.session_state:
         st.session_state.messages = []
 
-
-    model = st.selectbox("Choose a Bot", ["gpt-3.5-turbo", "gpt-4"])
+    model = st.selectbox("Choose a Bot", ["gpt-3.5-turbo", "gpt-4", "asst_IDB3oop3AS2Aoe5dZsjlvsak"])
 
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
     if prompt := st.chat_input("What is up?"):
-
         st.chat_message("user").markdown(prompt)
 
         st.session_state.messages.append({"role": "user", "content": prompt})
