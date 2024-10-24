51956a4fbd5a9ec0343fe6364b2caeec2479c4d7
Akhil Anilkumar <akhil.anilkumar@cubettech.com>
2024-10-24 12:29:48 +0530

    + assistant+3

--- stream.py
+++ stream.py
@@ -1,29 +1,34 @@
+from http.client import responses
 
 import streamlit as st
 import requests
 import os
+from openai import OpenAI
 from uuid import uuid4 as uid
+
+
+client = OpenAI()
 
 API_URL = "https://api.openai.com/v1/assistants/chat"
 API_KEY = os.getenv("OPENAI_API_KEY")
 
 
-def query_chatbot(messages, model):
-    headers = {
-        "Authorization": f"Bearer {API_KEY}",
-        "OpenAI-Beta": "assistants=v2",
-        "Content-Type": "application/json"
-    }
-    data = {
-        "model": model,
-        "messages": messages
-    }
-    response = requests.post(API_URL, headers=headers, json=data)
-
-    if response.status_code != 200:
-        return "Error: " + response.text
-
-    return response.json()['choices'][0]['message']['content']
+# def query_chatbot(messages, model):
+#     headers = {
+#         "Authorization": f"Bearer {API_KEY}",
+#         "OpenAI-Beta": "assistants=v2",
+#         "Content-Type": "application/json"
+#     }
+#     data = {
+#         "model": model,
+#         "messages": messages
+#     }
+#     response = requests.post(API_URL, headers=headers, json=data)
+#
+#     if response.status_code != 200:
+#         return "Error: " + response.text
+#
+#     return response.json()['choices'][0]['message']['content']
 
 
 def main():
@@ -43,7 +48,25 @@ def main():
 
         st.session_state.messages.append({"role": "user", "content": prompt})
 
-        response = query_chatbot(st.session_state.messages, model)
+        # response = query_chatbot(st.session_state.messages, model)
+        thread = client.beta.threads.create()
+        message = client.beta.threads.messages.create(
+            thread_id=thread.id,
+            role="user",
+            content=str(prompt)
+        )
+        run = client.beta.threads.runs.create_and_poll(
+            thread_id=thread.id,
+            assistant_id=model,
+        )
+        response = None
+        if run.status == 'completed':
+            response = client.beta.threads.messages.list(
+                thread_id=thread.id
+            )
+
+        else:
+            response=run.status
 
         with st.chat_message("assistant"):
             st.markdown(response)
